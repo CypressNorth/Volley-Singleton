00ba771d8732bf101b96d0f56c435066591d7d90
Matt Mombrea <mombrea@cypressnorth.com>
2014-03-04 11:55:32 -0500

    A sample of an Application class

---
+++ MyApplication.java
@@ -0,0 +1,29 @@
+package com.company.myapplication;
+
+import android.app.Application;
+import android.content.Context;
+
+import java.util.List;
+
+public class MyApplication extends Application {
+    private static MyApplication mInstance;
+    private static Context mAppContext;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        mInstance = this;
+
+        this.setAppContext(getApplicationContext());
+    }
+
+    public static MyApplication getInstance(){
+        return mInstance;
+    }
+    public static Context getAppContext() {
+        return mAppContext;
+    }
+    public void setAppContext(Context mAppContext) {
+        this.mAppContext = mAppContext;
+    }
+}
